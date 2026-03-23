db79e57607040ba7ee2a71deddc0100d7a93cbaa
Bela Lazar <ifj.bela.lazar@outlook.hu>
2026-03-23 16:09:39 +0100

    add spring boot backend files

---
+++ program/src/main/java/com/lazarbela/ikthesis/IkthesisApplication.java
@@ -0,0 +1,13 @@
+package com.lazarbela.ikthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkthesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkthesisApplication.class, args);
+	}
+
+}
---
+++ program/src/test/java/com/lazarbela/ikthesis/IkthesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazarbela.ikthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkthesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
